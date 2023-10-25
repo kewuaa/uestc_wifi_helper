31f1ecf71c66e0fc061dc9184217b5f947491787
kewuaa <1692525710@qq.com>
2023-10-25 15:09:47 +0800

    ignore error `INFO Error锛宔rr_code=2` which is because `The device is not within the scope of certification`

--- src/Program.cs
+++ src/Program.cs
@@ -200,6 +200,10 @@ static partial class UESTCWIFIHelper {
         res_dict = ParseResponse(res);
         ok = res_dict.error == "ok";
         if (!ok) {
+            if (res_dict.error_msg == "INFO Error锛宔r_code=2") {
+                Log("设备不在认证范围内");
+                return;
+            }
             throw new Exception($"Login failed: {res_dict.error_msg}");
         }
         Log($"\"{client_ip}\" user: {data.username} successfully login");
